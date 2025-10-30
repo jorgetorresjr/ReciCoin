e890a02b26a853d325f5adedf84bbe3d1fd49859
Jorge Luís Torres <jorgetorresjunior175@gmail.com>
2025-10-30 10:45:55 -0300

    1o commit

---
+++ src/main/java/org/ifpe/recicoin/ReciCoinApplication.java
@@ -0,0 +1,13 @@
+package org.ifpe.recicoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciCoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ifpe/recicoin/ReciCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ifpe.recicoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
